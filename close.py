621951823a757c83f7f0e93be6bdd915a82c00f7
Paul Sonnier <paul.sonnier@hotmail.fr>
2019-04-17 11:59:27 +0200

    comment fonc close

--- close.py
+++ close.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 def close():
+
+    ### ferme les ports series utilise par les appareils peripherique de la manip
     
-    ser.close()    
-    ser_temp.close()
-    ser_ox.close()
-    ser_mio.close() 
-    ser_licor.close()
-    
+    ser.close()    #gps
+    ser_temp.close()    #sbe38
+    ser_ox.close()  #optode
+    ser_mio.close() #miocard
+    ser_licor.close()   #Licor
+
+### execution de la procedure
+ 
 close()
